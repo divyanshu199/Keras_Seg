0c167acbc3a5bf3ba36b8ee67a51185612f50003
divyanshu199 <88285784+divyanshu199@users.noreply.github.com>
2023-05-11 15:51:17 +0530

    Update metrics.py

--- keras_segmentation/metrics.py
+++ keras_segmentation/metrics.py
@@ -12,27 +12,3 @@ def get_iou(gt, pr, n_classes):
         iou = float(intersection)/(union + EPS)
         class_wise[cl] = iou
     return class_wise
-def mean_iou(y_true, y_pred):
-    # Convert predictions to integer class values
-    y_pred = tf.argmax(y_pred, axis=-1)
-    y_pred = tf.cast(y_pred, tf.float32)
-
-    # Calculate Mean IoU
-    mean_iou = MeanIoU(num_classes=n_classes)
-    mean_iou.update_state(y_true, y_pred)
-    return mean_iou.result().numpy()
-
-def fw_iou(y_true, y_pred):
-    # Convert predictions to integer class values
-    y_pred = tf.argmax(y_pred, axis=-1)
-    y_pred = tf.cast(y_pred, tf.float32)
-
-    # Calculate frequency weights
-    class_counts = tf.reduce_sum(y_true, axis=(0, 1))
-    freq_weights = 1.0 / (class_counts + 1)
-
-    # Calculate FW IoU
-    intersection = tf.reduce_sum(y_true * y_pred, axis=(0, 1))
-    union = tf.reduce_sum(y_true + y_pred, axis=(0, 1)) - intersection
-    fw_iou = tf.reduce_sum(freq_weights * intersection) / tf.reduce_sum(freq_weights * union)
-    return fw_iou
