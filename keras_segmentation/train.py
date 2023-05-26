6d89180b5eec0d96489092c2fb8758a53b902bc9
divyanshu199 <88285784+divyanshu199@users.noreply.github.com>
2023-05-26 12:09:00 +0530

    Update train.py

--- keras_segmentation/train.py
+++ keras_segmentation/train.py
@@ -128,7 +128,7 @@ def train(model,
 
         model.compile(loss=loss_k,
                       optimizer=optimizer_name,
-                      metrics=[dice_coeff,'accuracy',MeanIoU(num_classes=n_classes)])
+                      metrics=[dice_coeff,'accuracy',MeanIoU(num_classes=2)])
 
     if checkpoints_path is not None:
         config_file = checkpoints_path + "_config.json"
